b499460b13733921b0c7dd58ee60fb2a0edc06a0
SteelTalonsTerminal <steeltalons6910@gmail.com>
2022-03-23 15:56:33 -0400

    added togglable solenoids.

--- src/main/cpp/Drivetrain.cpp
+++ src/main/cpp/Drivetrain.cpp
@@ -3,6 +3,8 @@
 
 //define the xbox controller.
 const frc::XboxController m_controller{CONTROLLER};
+bool sol1tog = 0;
+bool apressed = false;
 
 Drivetrain::Drivetrain() {
     //inverse one side of the drivetrain.
@@ -10,13 +12,33 @@ Drivetrain::Drivetrain() {
     pcmCompressor.EnableDigital();
     pcmCompressor.Disable();
     pcmCompressor.EnableDigital();
+    DoublePCM1.Set(frc::DoubleSolenoid::Value::kOff);
 }
 
 //arcade drive, left stick forward/backward right stick left/right.
 void Drivetrain::TuxDrive() {
     m_drivetrain.ArcadeDrive(-m_controller.GetLeftY(), -m_controller.GetRightX());
+    //checks to see if the a button has been pressed and toggles Solenoid group 1.
+    if(m_controller.GetAButton()) {
+        if(!apressed){
+            DSolenoid1Toggle();
+        }
+        apressed = true;
+    } else {
+        apressed = false;
+    }
 }
 
-void Drivetrain::Intake(){
+void Drivetrain::Intake() {
     m_intake.Set(0.75);
+}
+
+//toggle solenoid group 1.
+void Drivetrain::DSolenoid1Toggle() {
+    sol1tog = !sol1tog;
+    if(sol1tog) {
+        DoublePCM1.Set(frc::DoubleSolenoid::Value::kForward);
+    } else {
+        DoublePCM1.Set(frc::DoubleSolenoid::Value::kReverse);
+    }
 }
--- src/main/include/Drivetrain.h
+++ src/main/include/Drivetrain.h
@@ -7,12 +7,15 @@
 #include <frc/motorcontrol/MotorControllerGroup.h>
 #include <frc/motorcontrol/Spark.h>
 #include <frc/Compressor.h>
+#include <frc/Solenoid.h>
+#include <frc/DoubleSolenoid.h>
 
 class Drivetrain {
     public:
         Drivetrain();
         void TuxDrive();
         void Intake();
+        void DSolenoid1Toggle();
 
     private:
         //define intake motor
@@ -30,5 +33,7 @@ class Drivetrain {
         //create a differential drive using the two previously defined groups.
         frc::DifferentialDrive m_drivetrain{m_left, m_right};
 
+        //define compressor and solenoid groups.
         frc::Compressor pcmCompressor{PNEUMATICS, frc::PneumaticsModuleType::CTREPCM};
+        frc::DoubleSolenoid DoublePCM1{frc::PneumaticsModuleType::CTREPCM, 1, 2};
 };
